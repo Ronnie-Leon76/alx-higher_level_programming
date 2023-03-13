9c46394f5f181bef54495a91a93a15e2d1a0c186
Ronnie Leon <ronleon76@gmail.com>
2023-03-13 09:34:06 +0000

    formatted the code

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -5,39 +5,34 @@
  *
  * Return: 0 if not a palindrome or 1 if it is a palindrome
  */
-int is_palindrome(listint_t **head)
-{
-	listint_t *current = *head;
-	int i = 0, j = 0, len = 0, *arr;
+int is_palindrome(listint_t **head) {
+  listint_t *current = *head;
+  int i = 0, j = 0, len = 0, *arr;
 
-	if (!head || !*head)
-		return (1); /* An empty list is a palindrome */
-	while (current != NULL)
-	{
-		current = current->next;
-		len++;
-	}
-	arr = malloc(sizeof(int) * len);
-	if (!arr)
-		return (0);
-	current = *head;
-	while (current != NULL)
-	{
-		arr[i] = current->n;
-		current = current->next;
-		i++;
-	}
-	i--;
-	while (i > j)
-	{
-		if (arr[i] != arr[j])
-		{
-			free(arr);
-			return (0);
-		}
-		i--;
-		j++;
-	}
-	free(arr);
-	return (1);
+  if (!head || !*head)
+    return (1); /* An empty list is a palindrome */
+  while (current != NULL) {
+    current = current->next;
+    len++;
+  }
+  arr = malloc(sizeof(int) * len);
+  if (!arr)
+    return (0);
+  current = *head;
+  while (current != NULL) {
+    arr[i] = current->n;
+    current = current->next;
+    i++;
+  }
+  i--;
+  while (i > j) {
+    if (arr[i] != arr[j]) {
+      free(arr);
+      return (0);
+    }
+    i--;
+    j++;
+  }
+  free(arr);
+  return (1);
 }
