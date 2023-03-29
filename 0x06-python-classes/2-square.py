18e9a74d3f59b3af21a5f7aa3cafc72299a7ee17
Ronnie-Leon76 <ronleon76@gmail.com>
2023-03-28 19:16:18 -0700

    Size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size = 0):
+        """Initializes the data"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """getter method"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter Method"""
+        if type(value) is not int:
+            raise TypeError("Size must be an integer")
+        elif value < 0:
+            raise ValueError("Size must be >= 0")
+        else:
+            self.__size = value
+
