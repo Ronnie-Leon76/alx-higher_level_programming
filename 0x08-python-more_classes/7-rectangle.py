50afd21e6a30e2a7d4cb15c7b0631b3d8e5d266a
Ronnie-Leon76 <ronleon76@gmail.com>
2023-04-03 19:27:43 -0700

    change representation

---
+++ 0x08-python-more_classes/7-rectangle.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+"""Rectangle class"""
+
+
+class Rectangle:
+    """Rectangle class"""
+    number_of_instances = 0
+    print_symbol: any = '#'
+
+    def __init__(self, width=0, height=0):
+        """Initializes the data"""
+        self.height = height
+        self.width = width
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        """Getter methods"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter methods"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """getter method"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setter method"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        """Calculate area"""
+        return self.__height*self.__width
+
+    def perimeter(self):
+        """calculate perimeter"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return 2*(self.__height + self.__width)
+
+    def __str__(self):
+        """Prints the rectangle with the character #"""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        rect = ""
+        for row in range(self.__height):
+            for col in range(self.__width):
+                try:
+                    rect += str(self.print_symbol)
+                except Exception:
+                    rect += type(self).print_symbol
+            if row < self.__height - 1:
+                rect += "\n"
+        return (rect)
+
+    def __repr__(self):
+        """Returns a string representation of the rectangle"""
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __del__(self):
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
