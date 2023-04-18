7e3ef30bf9f27ce086e967c33c5e85f299ca54ea
Ronnie-Leon76 <ronleon76@gmail.com>
2023-04-17 19:18:36 -0700

    formatted

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-from .rectangle import Rectangle
+"""Module contains a square class"""
 
 
-"""Square class"""
+from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
     """Square class"""
     def __init__(self, size, x=0, y=0, id=None):
         """Initialize Data"""
+        self.size = size
+        self.x = x
+        self.y = y
+        self.id = None
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
