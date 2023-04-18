97141193829805e9c946746be3778a2c17863fba
Ronnie-Leon76 <ronleon76@gmail.com>
2023-04-17 19:04:11 -0700

    super() base constructor call

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -11,11 +11,11 @@ class Rectangle(Base):
     """Rectangle Class"""
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initialize Data"""
-        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+        super().__init__(id)
 
     @property
     def width(self):
