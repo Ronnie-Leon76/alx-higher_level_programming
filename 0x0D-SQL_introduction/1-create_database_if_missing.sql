8637b70a584cc20f31f8852164c6fbeb20b1162b
Ronnie-Leon76 <ronleon76@gmail.com>
2023-08-16 09:27:17 -0700

    Create a database

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,5 +1,3 @@
-/*
-This script creates the database hbtn_0c_0 
-If the database hbtn_0c_0 already exists, the script should not fail
-*/
+-- This script creates the database hbtn_0c_0 
+-- If the database hbtn_0c_0 already exists, the script should not fail
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
