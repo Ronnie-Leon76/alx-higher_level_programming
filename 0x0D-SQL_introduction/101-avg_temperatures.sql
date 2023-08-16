bf8f41e894e243d782a1a495919226731c0c43a7
Ronnie Leon <ronleon76@gmail.com>
2023-08-16 16:35:32 +0000

    commented properly

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,3 @@
-/*
-This script displays the average temperature (Fahrenheit) by city ordered by
-temperature (descending)
- */
+-- This script displays the average temperature (Fahrenheit) by city ordered by
+-- temperature (descending)
 SELECT city, AVG(value) OVER(PARTITION BY value) AS avg_temp FROM temperatures ORDER BY avg_temp DESC;
--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,5 +1,3 @@
-/*
-This script removes all records with a score <= 5 in the table second_table
-of the database hbtn_0c_0
- */
+-- This script removes all records with a score <= 5 in the table second_table
+-- of the database hbtn_0c_0
 DELETE FROM hbtn_0c_0.second_table WHERE score <= 5;
