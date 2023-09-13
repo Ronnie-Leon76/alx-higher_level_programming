3cf80ad8bb77c3ca4e81a3ebd250cbf830866399
Ronnie-Leon76 <ronleon76@gmail.com>
2023-09-13 23:27:45 +0300

    feat: Occurrences

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,9 +1,9 @@
 exports.nbOccurences = function (list, searchElement) {
-    let count = 0;
-    for (const element of list) {
-        if (element === searchElement) {
-        count++;
-        }
+  let count = 0;
+  for (const element of list) {
+    if (element === searchElement) {
+      count++;
     }
-    return count;
-}
+  }
+  return count;
+};
