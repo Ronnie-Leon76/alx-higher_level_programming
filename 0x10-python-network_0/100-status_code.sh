de7293bd3796e2137062bae91cb342ba4f5435b6
Ronnie-Leon76 <ronleon76@gmail.com>
2023-09-29 17:46:55 +0300

    feat: Only status code

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script sends a request to a URL passed as an argument, and displays only the status code of the response
-curl -sL -w "%{http_code}" "$1" -X GET
+curl -sL -w "%{http_code}" "$1"
