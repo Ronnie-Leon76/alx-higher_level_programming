ca76f845c8392d275b9263dffe18cc0378257e04
Ronnie-Leon76 <ronleon76@gmail.com>
2023-09-30 12:02:34 +0300

    feat: My Github!

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Script that takes your Github credentials (username and password)
+and uses the Github API to display your id
+"""
+import requests
+import sys
+if __name__ == "__main__":
+    response = requests.get('https://api.github.com/user',
+                            auth=(sys.argv[1], sys.argv[2]))
+    print(response.json().get('id'))
