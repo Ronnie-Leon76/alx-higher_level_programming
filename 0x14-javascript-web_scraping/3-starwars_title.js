7242ed8b6838e8cd6023bc8eb399b09aa44205e1
Ronnie-Leon76 <ronleon76@gmail.com>
2023-11-04 13:04:34 +0300

    Feat: Star wars movie title

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,12 @@
+#!/home/adeptschneiderthedev/.nvm/versions/node/v18.17.1/bin/node
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const json = JSON.parse(body);
+    console.log(json.title);
+  }
+});
