95619453ff54d1c93df53f972d7a6d6dc8aaf5c1
Ronnie-Leon76 <ronleon76@gmail.com>
2023-11-04 13:19:53 +0300

    Feat: Loripsum

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,17 @@
+#!/home/adeptschneiderthedev/.nvm/versions/node/v18.17.1/bin/node
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const file = process.argv[3];
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    fs.writeFile(file, body, 'utf-8', (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+});
