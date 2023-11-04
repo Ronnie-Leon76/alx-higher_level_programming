3f7275aa914db593e3e5f84beba299ad68137ef8
Ronnie-Leon76 <ronleon76@gmail.com>
2023-11-04 13:31:04 +0300

    Feat: Who was playing in this movie?

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,20 @@
+#!/home/adeptschneiderthedev/.nvm/versions/node/v18.17.1/bin/node
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/';
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const json = JSON.parse(body).characters;
+    for (let i = 0; i < json.length; i++) {
+      request(json[i], function (err, response, body) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    }
+  }
+});
